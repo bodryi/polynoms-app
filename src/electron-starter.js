b332bfabcf15eb5d97a89880e5e530278cea5c68
bodryi <vladmolodez@gmail.com>
2018-09-29 11:16:01 +0300

    initial commit, starter boilerplate

---
+++ src/electron-starter.js
@@ -0,0 +1,78 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const { app, BrowserWindow, Menu } = electron;
+
+let mainWindow;
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+function createWindow() {
+  mainWindow = new BrowserWindow({});
+  const startUrl =
+    process.env.ELECTRON_START_URL ||
+    url.format({
+      pathname: path.join(__dirname, '/../dist/polynoms-app/index.html'),
+      protocol: 'file:',
+      slashes: true,
+    });
+  mainWindow.loadURL(startUrl);
+
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  Menu.setApplicationMenu(mainMenu);
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+const mainMenuTemplate = [
+  {
+    label: 'Файл',
+    submenu: [
+      {
+        label: 'Выход',
+        accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click() {
+          app.quit();
+        },
+      },
+    ],
+  },
+];
+
+if (process.platform == 'darwin') {
+  mainMenuTemplate.unshift({});
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu: [
+      {
+        label: 'Toggle DevTols',
+        accelerator:
+          process.platform == 'darwin' ? 'Command+Shift+I' : 'Ctrl+Shift+I',
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        },
+      },
+      {
+        role: 'reload',
+      },
+    ],
+  });
+}
