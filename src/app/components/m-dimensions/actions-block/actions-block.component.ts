ba9d93e52eb08803aba5ab695a85d5b44ed37a0c
papko <papko@protei.ru>
2018-10-06 15:43:33 +0300

    actions power form connected to ngrx store

--- src/app/components/m-dimensions/actions-block/actions-block.component.ts
+++ src/app/components/m-dimensions/actions-block/actions-block.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { FormControl, FormGroup } from '@angular/forms';
+import * as fromRoot from '../../../store';
+import * as actions from '../../../store/actions/actions';
+import { select, Store } from '@ngrx/store';
+import { debounceTime, takeUntil } from 'rxjs/internal/operators';
 
 @Component({
   selector: 'actions-block',
   templateUrl: 'actions-block.component.html',
   styleUrls: ['actions-block.component.css'],
 })
+export class ActionsBlockComponent implements OnInit, OnDestroy {
+  powerForm: FormGroup;
 
-export class ActionsBlockComponent {
+  private ngUnsubscribe: Subject<void> = new Subject();
+
+  constructor(private store: Store<fromRoot.State>) {}
+
+  ngOnInit() {
+    this.powerForm = new FormGroup({
+      power: new FormControl(''),
+    });
+
+    this.store
+      .pipe(select(fromRoot.getN), takeUntil(this.ngUnsubscribe))
+      .subscribe((n: string) =>
+        this.powerForm.get('power').setValue(n, { emitEvent: false }),
+      );
+
+    this.powerForm
+      .get('power')
+      .valueChanges.pipe(takeUntil(this.ngUnsubscribe), debounceTime(100))
+      .subscribe((value: string) =>
+        this.store.dispatch(new actions.SetN(value)),
+      );
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
 }
--- src/app/store/actions/reducer.ts
+++ src/app/store/actions/reducer.ts
@@ -1,27 +1,44 @@
 import { IBtnsState } from '../../models/btns-state.model';
 import * as Action from './actions';
+import * as MainAction from '../main/actions';
 
 export interface State {
-  n: number;
+  n: Array<string>;
+  matrixSize: number;
 }
 
 const initialState: State = {
-  n: null,
+  n: ['', ''],
+  matrixSize: null,
 };
+
+function getIndex(matrixSize: number): number {
+  return (matrixSize - 4) / 2;
+}
 
 export function reducer(state = initialState, action: any): State {
   switch (action.type) {
     case Action.SET_N:
       return {
         ...state,
-        n: action.payload,
+        n: state.n.map(
+          (c, index) =>
+            index === getIndex(state.matrixSize) ? action.payload : c,
+        ),
       };
+
+    case MainAction.SET_MATRIX_SIZE:
+      return {
+        ...state,
+        matrixSize: action.payload,
+      };
+
     default:
       return state;
   }
 }
 
-export const getN = (state: State) => state.n;
+export const getN = (state: State) => state.n[getIndex(state.matrixSize)];
 export const getButtonsState = (
   state: State,
   testVectorAValid: boolean,
